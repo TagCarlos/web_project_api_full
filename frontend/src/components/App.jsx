541e5d37f5e08dd06db798293407614e946b2cd7
TagCarlos <charlyaragon@hotmail.com>
2026-05-27 19:44:50 -0600

    debug, verificando variables de entorno

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -18,6 +18,8 @@ import ProtectedRoute from "./Main/components/ProtectedRoute/ProtectedRoute.jsx"
 import InfoTooltip from "./Main/components/InfoTooltip/InfoTooltip.jsx";
 
 function App() {
+  console.log("API URL:", process.env.REACT_APP_API_URL);
+  console.log("API:", process.env.REACT_APP_API);
   const api = new Api({
     baseUrl: process.env.REACT_APP_API,
     headers: {
